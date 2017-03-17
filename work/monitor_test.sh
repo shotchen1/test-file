24c33c2ce319cc8fb6d4be59e08b02acf06ff93f
shotchen <xuelinchen@126.com>
2017-03-17 18:11:33 +0800

    Update monitor_test.sh

--- work/monitor_test.sh
+++ work/monitor_test.sh
@@ -16,10 +16,12 @@ exec 2>>$log_file
 while true
 do
   time=`date "+%Y-%m-%d %H:%M:%S"`
-  if
+  
   cpuuse=`top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ len=split($1, vs, ","); v=vs[len]; sub("%", "", v); printf  100-v }'`
-  mem=`free -m|grep Mem|awk '{print ($3-$6-$7)/$2*100}'`
-  disk=`df -h | grep /dev/sda1|awk '{print $5}'|cut -c 1-2`
+  meminfo=`free -m|grep Mem|awk '{print ($3-$6-$7)/$2*100}'`
+  diskinfo=`df -h | grep /dev/sda1|awk '{print $5}'|cut -c 1-2`
+  mysqlinfo=`top -b -n2 |fgrep mysqld|tail -1`
+  mysqlres=`echo "$mysqlinfo"|awk '{print $6}'|
   rx=`ifconfig eth2 | sed -n "8p" | awk '{print $2}' | cut -c 7-`
   tx=`ifconfig eth2 | sed -n "8p" | awk '{print $6}' | cut -c 7-`
   sleep 2
